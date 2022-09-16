850f66e0970fcf1b7dc5dd76a2e9691a2aae39cf
shalash23 <103309490+shalash23@users.noreply.github.com>
2022-09-16 22:48:49 +0200

    Javascript enabled

--- main.js
+++ main.js
@@ -4,14 +4,16 @@ const observer = new IntersectionObserver((entries) =>{
     entries.forEach((entry) =>{
         console.log(entry);
         if(entry.isIntersecting){
-            entry.target.classList.add('.show')
+            entry.target.classList.add('show')
+            console.log(entry);
             
         }else{
-            entry.target.classList.remove('.show')
+            entry.target.classList.remove('show')
+            console.log(entry);
         }
     });
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
 
-hiddenElements.forEach((el)=> observer.observe(el) );
+hiddenElements.forEach((el)=> observer.observe(el));
